e00ae99712809dadd9582ba3f020b6b215a8ebe1
Gaurav <gauravrawatgro26@gmail.com>
2022-12-28 20:24:17 +0530

    moved servlet initialiser to new file

--- src/main/java/com/example/employee_management/EmployeeManagementApplication.java
+++ src/main/java/com/example/employee_management/EmployeeManagementApplication.java
@@ -2,13 +2,16 @@ package com.example.employee_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 @SpringBootApplication
-public class EmployeeManagementApplication extends SpringBootServletInitializer {
+public  class EmployeeManagementApplication{
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
+
 }
